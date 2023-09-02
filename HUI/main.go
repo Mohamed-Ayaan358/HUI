49936734a123c8c1fc568544cb2bdcb7d36272ff
Mohamed Ayaan <ayaan35813@gmail.com>
2023-09-02 19:09:10 +0530

    Css

---
+++ HUI/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
+)
+
+// Struct to represent a node in the parse tree
+
+type Node struct {
+	Name     string
+	Content  map[string]string
+	Children []*Node
+}
+
+var css []string
+
+// myDictionary := make(map[string]int)
+
+func readHtmlFromFile(fileName string) (string, error) {
+
+	bs, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
+func indexOf(slice []string, target string) int {
+	for i, v := range slice {
+		if v == target {
+			return i
+		}
+	}
+	return -1 // Return -1 if the target is not found in the slice
+}
+
+func main() {
+	fileName := "index.html"
+	text, err := readHtmlFromFile(fileName)
+
+	Parser(text)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func CSSextractor(CSSlines []string, CSSnum int) map[string]string {
+	CSSselector := make(map[string]string)
+
+	for _, lin := range CSSlines[CSSnum:] {
+		trimmedl := strings.TrimSpace(lin)
+		css = append(css, trimmedl)
+		if "</style>" == trimmedl {
+			break
+		}
+	}
+
+	css = css[1 : len(css)-1]
+
+	re1 := regexp.MustCompile(`\w+ {`)
+	re2 := regexp.MustCompile(`}`)
+
+	start := 0
+	end := 0
+
+	// var match string
+	for n, val := range css {
+		res1 := re1.FindString(val)
+		res2 := re2.FindString(val)
+
+		if res1 != "" {
+			start = n
+		}
+		if res2 != "" {
+			end = n
+			tag := css[start][0 : len(css[start])-2]
+			CSSselector[tag] = strings.Join(css[start+1:end], "")
+		}
+
+	}
+	return CSSselector
+
+}
+
+func Parser(text string) {
+	lines := strings.Split(text, "\n")
+
+	for num, line := range lines {
+		trimmedLine := strings.TrimSpace(line)
+
+		if "<style>" == trimmedLine {
+			CSSselector := CSSextractor(lines, num)
+			for key, value := range CSSselector {
+				val := strings.Split(value, ";")
+				fmt.Println(key, val)
+			}
+		}
+	}
+}
